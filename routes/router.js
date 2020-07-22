ef62142ad103364bead767db7f92393ddf5c1c55
rachOS <g.chamekh@e-nautia.com>
2020-07-22 19:16:46 +0200

    basic CRUD working on groups.js

--- routes/router.js
+++ routes/router.js
@@ -5,11 +5,13 @@ const router = express.Router();
 // import CRUD
 const users = require("./users");
 const foods = require("./foods");
+const groups = require("./groups");
 const brands = require("./brands");
 
 // init route
 router.use("/users", users);
 router.use("/foods", foods);
+router.use("/groups", groups);
 router.use("/brands", brands);
 
 // export
