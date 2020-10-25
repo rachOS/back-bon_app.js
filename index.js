6d88a8bdb7cd6824d624a3beb3ef37c4a710a7b3
rachOS <g.chamekh@e-nautia.com>
2020-10-25 11:45:26 +0100

    fix: conflict on index.js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-const port = process.env.PORT || 8000;
-const cors = require("cors");
-const routes = require("./routes/router");
+const port = process.env.PORT;
+const dotenv = require('dotenv');
+const cors = require('cors');
+const routes = require('./routes');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-<<<<<<< HEAD
 app.use('/api', routes);
 
 // conditional test for Heroku PORT
 if (port == null || port == "") {
     port = 8000;
   }
-=======
-app.use("/api", routes);
->>>>>>> develop
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(`listen on port ${port}`);
-});
+  if(err) {
+    console.log(err)
+  }
+  console.log('Super :)')
+})
+
+
