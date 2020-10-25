9833d0483bee189b2048fe7b5881cb5d85f1fdae
rachOS <g.chamekh@e-nautia.com>
2020-10-25 10:42:57 +0100

    fix: format document with Prettier

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config()
-const port = process.env.PORT || 8000  ;
-const cors = require('cors');
-const routes = require('./routes/router');
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+const routes = require("./routes/router");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api', routes);
-
+app.use("/api", routes);
 
 app.listen(port, (err) => {
-  if(err) {
-    console.log(err)
-  }
-  console.log(`listen on port ${port}`)
-})
-
-
+    if (err) {
+        console.log(err);
+    }
+    console.log(`listen on port ${port}`);
+});
