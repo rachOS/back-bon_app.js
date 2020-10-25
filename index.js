79c4ebb0256d5365ab198ce734129352d5dc8610
rachOS <g.chamekh@e-nautia.com>
2020-10-25 11:49:20 +0100

    fix: routing and change console.log message when listen port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+let port = process.env.PORT;
 const dotenv = require('dotenv');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes/router');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -20,7 +20,7 @@ app.listen(port, (err) => {
   if(err) {
     console.log(err)
   }
-  console.log('Super :)')
+  console.log(`app listen on port ${port}`)
 })
 
 
