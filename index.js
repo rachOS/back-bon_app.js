b815aa63f75ee2521d55c57b5bee885d2640fb41
rachOS <g.chamekh@e-nautia.com>
2021-02-17 10:36:02 +0100

    fix: credentials origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const routes = require("./routes/router");
 
-const local = `http://localhost:${port}`;
+const local = `http://localhost:3000`;
 const remote = "https://front-bonapp.herokuapp.com";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
