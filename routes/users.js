7ac228d82981da4081b013ed9005179b3dda0320
rachOS <g.chamekh@e-nautia.com>
2021-06-13 18:20:19 +0200

    [FEATURE]: write query SQL JOIN
    get and post food_recipe
    get and post user_recipe

--- routes/users.js
+++ routes/users.js
@@ -73,4 +73,47 @@ router.delete("/:idUser", (req, res) => {
   });
 });
 
+/** user_recipe */
+router.get("/:idUser/recipes", async (req, res) => {
+  try {
+    const {idUser} = req.params
+    const sql = "SELECT u.firstname, u.lastname, u.id AS user_id, r.id AS recipe_id, r.name AS recipe_name, r.calories AS recipe_cals\
+    FROM user AS u\
+    JOIN user_recipe AS ur ON (u.id=ur.user_id)\
+    JOIN recipe AS r ON (r.id=ur.recipe_id)\
+    WHERE u.id = ? \
+    "
+    await connection.query(sql, [idUser], async (error, result) => {
+      if (error) {
+        return await res.status(500).send(error);
+        
+     } else {
+        return await res.status(200).json(result);
+        
+     }
+        
+    })
+  } catch (error) {
+    await console.error(error);
+  }
+})
+
+router.post("/recipes", async (req, res) => {
+  try {
+    const {userID} = req.params
+    const sql = "INSERT INTO `user_recipe` SET ? "
+    const data =  await req.body
+    await connection.query(sql, [data], (error, result) => {
+
+      if (error) {
+        return res.status(500).send(error);
+      } else {
+        return res.status(200).json(result);
+      }
+  
+    })
+  } catch (error) {
+     console.error(error);
+  }
+})
 module.exports = router;
